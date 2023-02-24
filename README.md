129dde37622b0edab6b8fcfc7bf9bda85b89db52
programindfr <112480997+programindfr@users.noreply.github.com>
2023-02-24 14:35:31 +0100

    update

--- README.md
+++ README.md
@@ -9,21 +9,23 @@
   * ssh keys setup
   * fail2ban
   * ufw
+  * links:
     https://raspberrytips.com/security-tips-raspberry-pi/
     https://www.digitalocean.com/community/tutorials/how-to-configure-ssh-key-based-authentication-on-a-linux-server
  ## virtualisation
   * debian iso auto install (may some other to ?)
-    https://fai-project.org/doc/man/fai-diskimage.html
+    link: https://fai-project.org/doc/man/fai-diskimage.html
   * qemu kvm settings
-    https://www.qemu.org/docs/master/
-  * main c executable
-    Make sure to compile with `-luuid`
-    https://www.gnu.org/software/libc/manual/html_node/System-Calls.html
+    link: https://www.qemu.org/docs/master/
+  * main c executable.
+    Make sure to compile with `-luuid`.
+    links: https://www.gnu.org/software/libc/manual/html_node/System-Calls.html
     https://man7.org/linux/man-pages/man2/syscalls.2.html
     https://www.geeksforgeeks.org/multithreading-in-c/
     https://stackoverflow.com/questions/51053568/generating-a-random-uuid-in-c
   * service file
-    https://www.linuxtricks.fr/wiki/systemd-creer-des-services-timers-unites
+    link: https://www.linuxtricks.fr/wiki/systemd-creer-des-services-timers-unites
  ## access
-  * installation script
+  * installation script.
+    fai-mk-configspace
   * python flask website
--- tinyvps/main.c
+++ tinyvps/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <uuid/uuid.h>
 
 
 
-void create_vm_disk_img();
+void add(char *strout, char *strin1, char *strin2);
+void create_vm(char *newargv[]);
+void dcreate_vm();
+void run_vm();
+void drun_vm();
+void run_vms();
 
 
 
@@ -17,16 +21,48 @@ main(int argc, char *argv[])
 
 
 void
-create_vm_disk_img()
+add(char *strout, char *strin1, char *strin2)   /* concat two string in the first /!\ buffer overflow can occur */
 {
-    char *newargv[] = { NULL, NULL };
+    int i = 0;
+    int j = 0;
+
+    while (strin1[i] != '\0') {
+        strout[i] = strin1[i];
+        i++;
+    }
+
+    while (strin2[j] != '\0') {
+        strout[i + j] = strin2[j];
+        j++;
+    }
+
+    strout[i + j] = '\0';
+}
+
+
+void
+create_vm(char *newargv[])  /* create a vm disk image with https://fai-project.org/doc/man/fai-diskimage.html */
+{
     char *newenvp[] = { "FAI_BASEFILEURL=https://fai-project.org/download/basefiles/", NULL };
+
+    execve("fai-diskimage", newargv, newenvp);
+    perror("execve");   /* execve() returns only on error */
+}
+
+
+void
+dcreate_vm()    /* create a vm disk image named with uuid and setup a default config */
+{
     uuid_t binuuid; /* uuid in binary format */
-    char uuid[37];
+    char uuid[37];  /* 36 char uuid and \0 */
 
     uuid_generate_random(binuuid);
     uuid_unparse(binuuid, uuid);
 
-    execve("./", newargv, newenvp); /* exact command needed */
-    perror("execve");   /* execve() returns only on error */
+    char imagename[41]; /* 36 + 4 + 1 */
+    add(imagename, uuid, ".raw");
+
+    char *newargv[] = { "fai-diskimage", "-vu", uuid, "-S5G", "-cDEBIAN,STRETCH64,AMD64,FAIBASE,GRUB_PC,DHCPC,DEMO,CLOUD,LAST", imagename, NULL };  /* looking for -c in doc */
+
+    create_vm(newargv);   
 }
