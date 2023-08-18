4193500b3072b70262e12641966bee92ed14194a
programindfr <mathias.rivet73@orange.fr>
2023-08-19 00:19:53 +0200

    i need to finish so i'm making some major changes

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# tinyVPS
- This project is a VPS which aims to consume few resources.
- 
- ## memo
+# TinyVPS
+  This project is a VPS which aims to consume few resources. This will be in tree parts: the vps script itself, an installation script and a security set-up script. At this moment i don't have the necessary time to continue with the C programming language so i'm switched to BASH. Moreover i would like to build a website to control Tinyvps with Python and Flask framework.
+
+# features
+  ## security
+  * [unattended upgrades](https://wiki.debian.org/UnattendedUpgrades)
+  * [disable root login, fail2ban, ufw](https://raspberrytips.com/security-tips-raspberry-pi/)
+  * [configure ssh key based authentication](https://www.digitalocean.com/community/tutorials/how-to-configure-ssh-key-based-authentication-on-a-linux-server)
+
+  ## virtualisation
+  * create disk image with [fai api](https://fai-project.org/doc/api.html)
+  * emulate the machine with [qemu kvm](https://www.qemu.org/docs/master/)
+  * in c compile with `-luuid`. [System-Calls](https://www.gnu.org/software/libc/manual/html_node/System-Calls.html), [syscalls.2](https://man7.org/linux/man-pages/man2/syscalls.2.html), [multithreading](https://www.geeksforgeeks.org/multithreading-in-c/), [generating-a-random-uuid-in-c](https://stackoverflow.com/questions/51053568/generating-a-random-uuid-in-c).
+
+  ## installation
+  * install tools
+  * [systemd creer des services](https://www.linuxtricks.fr/wiki/systemd-creer-des-services-timers-unites)
+
+# memo
   ```
   qemu-system-x86_64 -drive file=server.raw,format=raw -m 2G -enable-kvm -smp 4 -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22
   ```
@@ -13,33 +28,3 @@
   ```
   https://fai-project.org/cgi/faime.cgi?type=cloud;disksize=3;format=raw.zst;hostname=uuid;username=debian;userpw=debian;partition=ONE;keyboard=fr;suite=bookworm;cl5=SSH_SERVER;cl6=STANDARD;cl8=REBOOT;sbm=2
   ```
-
-
-# roadmap
- ## security
-  * security auto update
-  * disable root login (change port ?)
-  * ssh keys setup
-  * fail2ban
-  * ufw
-  * links:
-    [security-tips](https://raspberrytips.com/security-tips-raspberry-pi/)<br>
-    [configure-ssh-key-based-authentication](https://www.digitalocean.com/community/tutorials/how-to-configure-ssh-key-based-authentication-on-a-linux-server)
- ## virtualisation
-  * debian iso auto install (may some other to ?)
-    link: [ai-diskimage](https://fai-project.org/doc/man/fai-diskimage.html)<br>
-    [fai-api](https://fai-project.org/doc/api.html)
-  * qemu kvm settings
-    link: [qemu-doc](https://www.qemu.org/docs/master/)
-  * main c executable.
-    Make sure to compile with `-luuid`.
-    links: [System-Calls](https://www.gnu.org/software/libc/manual/html_node/System-Calls.html)<br>
-    [syscalls.2](https://man7.org/linux/man-pages/man2/syscalls.2.html)<br>
-    [multithreading](https://www.geeksforgeeks.org/multithreading-in-c/)<br>
-    [generating-a-random-uuid-in-c](https://stackoverflow.com/questions/51053568/generating-a-random-uuid-in-c)
-  * service file
-    link: [systemd-creer-des-services](https://www.linuxtricks.fr/wiki/systemd-creer-des-services-timers-unites)
- ## access
-  * installation script.<br>
-    fai-mk-configspace
-  * python flask website
--- model/tinyvps.sh
+++ model/tinyvps.sh
@@ -1,1 +1,143 @@
 #!/bin/bash
+
+_baseurl="https://fai-project.org/cgi/faime.cgi?"       # API base url
+_type="cloud"                                           # Install type
+_disksize="3"                                           # Disk size in gigabytes
+_format="raw.zst"                                       # Image format
+_uuid=""                                                # Name of the vm (uuid) (name of the disk file)
+_username="debian"                                      # Username of the vm
+_userpw="debian"                                        # Password for this user
+_partition="ONE"                                        # Partition setting
+_keyboard="fr"                                          # Keyboard layout
+_suite="bookworm"                                       # Vm distro
+_variables="cl5=SSH_SERVER;cl6=STANDARD;cl8=REBOOT"     # Some settings
+_varend="sbm=2"                                         # Idk the purpuse of this var
+_ram="512M"                                             # Ram size with unit (M/G)
+_core="1"                                               # Number of allocated cores for the vm
+_port="5555:22"                                         # Port forwarding host:guest,...
+
+# Link will look like this (https://fai-project.org/doc/api.html):
+# https://fai-project.org/cgi/faime.cgi?
+# type=cloud
+# ;disksize=3
+# ;format=raw.zst
+# ;hostname=uuid
+# ;username=debian
+# ;userpw=debian
+# ;partition=ONE
+# ;keyboard=fr
+# ;suite=bookworm
+# ;cl5=SSH_SERVER;cl6=STANDARD;cl8=REBOOT
+# ;sbm=2
+
+usage(){
+    echo "Usage: $0
+    [-C] Create a new virtual machine
+    [-b arg] The API base url
+    [-t arg] Select the install type
+    [-d arg] Set the disk size in gigabytes (less than 99)
+    [-i arg] Select the disk image format
+    [-u arg] Set the name of the virtual machine (uuid) (name of the disk file)
+    [-n arg] Set the username of the virtual machine user
+    [-p arg] Set the password for the virtual machine user
+    [-P arg] Select the partition type
+    [-k arg] Select the keyboard layout
+    [-s arg] Select the virtual machine distro
+    [-v arg] Select the string variables
+    [-V arg] Another string variable but idk the purpuse of this one...
+    [-R arg] Run the associated virtual machine
+    [-r arg] Set the amount of RAM with the unit (M/G) for the virtual machine
+    [-c arg] Set the number of allocated cores for the virtual machine
+    [-f hostPort1:guestPort1,hostPort2:guestPort2,...] Add port forwarding
+    [-F] Use the config file to set-up this mess
+    [-h] Show this message
+    "
+    exit 1
+}
+
+createVmFlag=0          # C flag
+runVmFlag=0             # R flag
+useConfigFileFlag=0     # F flag
+
+while getopts "Cb:t:d:i:u:n:p:P:k:s:v:V:R:r:c:f:F?h" OPTION; do
+    case "$OPTION" in
+        C)
+            createVmFlag=1
+            ;;
+
+        b)
+            _baseurl="$OPTARG"
+            ;;
+
+        t)
+            _type="$OPTARG"
+            ;;
+
+        d)
+            _disksize="$OPTARG"
+            ;;
+
+        i)
+            _format="$OPTARG"
+            ;;
+
+        u)
+            _uuid="$OPTARG"
+            ;;
+
+        n)
+            _username="$OPTARG"
+            ;;
+
+        p)
+            _userpw="$OPTARG"
+            ;;
+
+        P)
+            _partition="$OPTARG"
+            ;;
+
+        k)
+            _keyboard="$OPTARG"
+            ;;
+
+        s)
+            _suite="$OPTARG"
+            ;;
+
+        v)
+            _variables="$OPTARG"
+            ;;
+
+        V)
+            _varend="$OPTARG"
+            ;;
+
+        R)
+            runVmFlag=1
+            _uuid="$OPTARG"
+            ;;
+
+        r)
+            _ram="$OPTARG"
+            ;;
+
+        c)
+            _core="$OPTARG"
+            ;;
+
+        f)
+            _port="$OPTARG"
+            ;;
+
+        F)
+            useConfigFileFlag=1
+            ;;
+
+        ? | h | *)
+            usage
+            ;;
+    esac
+done
+shift "$(($OPTIND - 1))"
+
